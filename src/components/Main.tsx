371d984467821dac75cd83fba23f13b39d06d405
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-10-06 12:02:14 +0200

    base structure

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import type { JSX } from "react"
+import ContactsPage from "../pages/ContactsPage"
+
+export default function Main(): JSX.Element {
+    return (
+        <main>
+           <ContactsPage/>
+        </main>
+    )
+}
---
+++ src/pages/ContactsPage.tsx
@@ -0,0 +1,7 @@
+import type { JSX } from "react"
+
+export default function ContactsPage(): JSX.Element {
+    return (
+        <p>Contacts Page</p>
+    )
+}
